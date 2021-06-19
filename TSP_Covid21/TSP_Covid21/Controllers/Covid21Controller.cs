f58fcfd189790442b1f28596a482d1500734c4e9
trisnguyen2511 <75766468+trisnguyen2511@users.noreply.github.com>
2021-06-20 00:33:11 +0700

    Revert "fix 3"
    This reverts commit 7bd5dea3a6176f941552e14850ea8ce9818aa196.

---
+++ TSP_Covid21/TSP_Covid21/Controllers/Covid21Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSP_Covid21.Controllers
+{
+    public class Covid21Controller : Controller
+    {
+        // GET: Covid21
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
