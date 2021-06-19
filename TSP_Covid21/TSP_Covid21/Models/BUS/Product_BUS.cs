7c30628130ad70ec7422e260fc76840c5757a58d
trisnguyen2511 <75766468+trisnguyen2511@users.noreply.github.com>
2021-06-20 00:34:38 +0700

    Revert "fix 2"
    This reverts commit 8ce48c755592ad2f0c0a5a484237e734da21fdcd.

--- TSP_Covid21/TSP_Covid21/Models/BUS/Product_BUS.cs
+++ TSP_Covid21/TSP_Covid21/Models/BUS/Product_BUS.cs
@@ -18,14 +18,7 @@ namespace TSP_Covid21.Models.BUS
 
         public IEnumerable<PRODUCT> loadProductNew()
         {
-            var date = DateTime.Now;
-            var result = db.PRODUCTs.OrderByDescending(t => t.DATEADD).Take(5);
-            return result;
-        }
-
-        public IEnumerable<PRODUCT> loadProductTop()
-        {
-            var result = db.PRODUCTs.OrderByDescending(t => t.PRODUCTVIEW).Take(5);
+            var result = db.PRODUCTs.ToList();
             return result;
         }
 
