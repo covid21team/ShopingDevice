d0a5f705769851839c99245ff57ff00197d8ea03
nguyenquangsy <83626285+nguyenquangsy@users.noreply.github.com>
2021-06-20 07:53:28 +0700

    AJAX and text

---
+++ AJAX/Source_AJAX/testAJAX/Controllers/AJAXController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testAJAX.Controllers
+{
+    public class AJAXController : Controller
+    {
+        // GET: AJAX
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult loadDataAjax()
+        {
+            return Content("phus");
+        }
+        
+        public ActionResult loadDataAjaxBeginForm(string text)
+        {
+            return Content(text); 
+        }
+
+        public ActionResult loadAjaxJQuery(int a, int b)
+        {
+            return Content((a+b).ToString());
+        }
+
+        public ActionResult loadIndex()
+        {
+            System.Threading.Thread.Sleep(2000);
+            return PartialView("hihi");
+        }
+    }
+}
