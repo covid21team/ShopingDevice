6e064791bfe57b30483e8278b30f8569ec00f279
TruongGiaPhu <85993254+TruongGiaPhu@users.noreply.github.com>
2021-06-21 22:06:17 +0700

    cart

--- Font_end_Shop/admin/js/table.js
+++ Font_end_Shop/admin/js/table.js
@@ -2,9 +2,13 @@
 // Get the modal
 var modal = document.getElementById('id01');
 
+var modal1 = document.getElementById('id02');
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == modal||event.target==modal1) {
         modal.style.display = "none";
+        modal1.style.display = "none";
     }
 }
+
