906e6785de32bfeca99cbf38616d031ccd6f143e
TruongGiaPhu <85993254+TruongGiaPhu@users.noreply.github.com>
2021-06-22 11:36:56 +0700

    admin forn-end tri ngu

--- admin/js/table.js
+++ admin/js/table.js
@@ -16,5 +16,25 @@ function Functionclick(){
     modal1.style.display = "none";
 }
 
+function Functionclick1(){
+    var modal4 = document.getElementById('table1');
+    modal4.style.display = "block";
+
+}
+// function Functionclick2(){
+//     var modal1 = document.getElementById('id05');
+//     var modal = document.getElementById('id04');
+//     var modal2 = document.getElementById('id03');
+ 
+//     modal1.style.display = "block";
+//     modal2.style.display = "block";
+//     modal.style.display = "none";
+// }
+function Functionclick3(){
+    var modal1 = document.getElementById('id03');
+ 
+    modal1.style.display = "block";
+   
+}
 
 
