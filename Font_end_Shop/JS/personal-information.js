4f872823bab3008c219522aff2c93c5ecc9a62d0
TruongGiaPhu <85993254+TruongGiaPhu@users.noreply.github.com>
2021-06-23 11:03:27 +0700

    admin

--- Font_end_Shop/JS/personal-information.js
+++ Font_end_Shop/JS/personal-information.js
@@ -1,6 +1,6 @@
 
 function myFunction() {
-  var checkBox = document.getElementById("myCheck");
+  var checkBox = document.getElementById("id7");
   var text = document.getElementById("passan");
   if (checkBox.checked == true){
     text.style.display = "block";
@@ -32,3 +32,46 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+
+  function openthem(){
+    var modal = document.getElementById('themthongtingiaohang');  
+    var modal1 = document.getElementById('tatbangiaohang');
+    modal.style.display = "block";
+    modal1.style.display = "none";
+
+}
+function tatthem(){
+  var modal = document.getElementById('themthongtingiaohang');  
+  var modal1 = document.getElementById('tatbangiaohang');
+  modal.style.display = "none";
+  modal1.style.display = "block";
+
+}
+function okthem(){
+  var modal = document.getElementById('themthongtingiaohang');  
+  var modal1 = document.getElementById('tatbangiaohang');
+  modal.style.display = "none";
+  modal1.style.display = "block";
+
+}
+function suathem(){
+  var modal = document.getElementById('themthongtingiaohang');  
+  var modal1 = document.getElementById('tatbangiaohang');
+  modal.style.display = "block";
+  modal1.style.display = "none";
+
+}
+// function openthem(evt, cityName) {
+//   var i, tabcontent, tablinks;
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//   document.getElementById(cityName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
