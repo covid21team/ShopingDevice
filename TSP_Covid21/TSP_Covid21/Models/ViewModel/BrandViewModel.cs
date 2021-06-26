2834f88b210b9d2cddd1fa8af4793f6c58a9fcab
nguyenquangsy <83626285+nguyenquangsy@users.noreply.github.com>
2021-06-26 19:59:51 +0700

    up source

---
+++ TSP_Covid21/TSP_Covid21/Models/ViewModel/BrandViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TSP_Covid21.Models.ShopEntity;
+
+namespace TSP_Covid21.Models.ViewModel
+{
+    public class BrandViewModel
+    {
+        public int BrandId { set; get; }
+        public string BrandName { set; get; }
+
+        public virtual ICollection<PRODUCT> Product { set; get; }
+
+        public virtual ICollection<TEMPPRODUCT> TempProduct { set; get; }
+    }
+}
