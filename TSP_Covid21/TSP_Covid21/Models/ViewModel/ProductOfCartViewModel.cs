756243969289a4ce75aec0cdec5f65c5341c661f
nguyenquangsy <83626285+nguyenquangsy@users.noreply.github.com>
2021-06-30 13:20:31 +0700

    up

---
+++ TSP_Covid21/TSP_Covid21/Models/ViewModel/ProductOfCartViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TSP_Covid21.Models.ViewModel
+{
+    public class ProductOfCartViewModel
+    {
+        public int ProductId { set; get; }
+        public string ProductName { set; get; }
+        public string MainPic { set; get; }
+        public Nullable<int> ProductPrice { set; get; }
+        public Nullable<int> ProductAmount { set; get; }
+
+        // Giúp kiểm tra sản phẩm có muốn mua hay không
+        public Nullable<bool> Status { get; set; }
+    }
+}
