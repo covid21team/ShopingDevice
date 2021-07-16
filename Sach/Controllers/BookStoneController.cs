2ef4d0c53af6341b38ffc5868fa6ea72cb9f9888
TruongGiaPhu <85993254+TruongGiaPhu@users.noreply.github.com>
2021-07-16 14:29:41 +0700

    sach

---
+++ Sach/Controllers/BookStoneController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sach.Controllers
+{
+    public class BookStoneController : Controller
+    {
+        // GET: BookStone
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sach/Models/Bus/Books.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sach.Models;
+
+namespace Sach.Models.Bus
+{
+    public class Books
+    {
+        public IEnumerable<NHAXUATBAN> loadNXB()
+        {
+            QLBANSACHEntities db = new QLBANSACHEntities();
+            var result = db.NHAXUATBANs.ToList();
+            return result;
+        }
+        public IEnumerable<CHUDE> loadChuDe()
+        {
+            QLBANSACHEntities db = new QLBANSACHEntities();
+            var result = db.CHUDEs.ToList();
+            return result;
+        }
+        public IEnumerable<SACH> loadSach()
+        {
+            QLBANSACHEntities db = new QLBANSACHEntities();
+            var result = db.SACHes.ToList();
+            return result;
+        }
+    }
+}
